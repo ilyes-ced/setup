6e102bbc7a34dbcf6847c44c914ee22359bbd5b6
ilyes-ced <random_dude_233@proton.me>
2023-08-26 13:57:20 +0100

    fix

--- archcraft/bspwm/themes/pywall_hack/theme.bash
+++ archcraft/bspwm/themes/pywall_hack/theme.bash
@@ -4,25 +4,28 @@
 # Hack Theme
 # ------------------------------------------------------------------------------
 
+
+. "${HOME}/.cache/wal/colors.sh"
+
 # Colors
-background='#01161b'
-foreground='#66FFFF'
-color0='#01161b'
-color1='#007b82'
-color2='#028c94'
-color3='#039ca4'
-color4='#04acb5'
-color5='#05bbc5'
-color6='#06ccd7'
-color7='#a3b5b8'
-color8='#01161b'
-color9='#007b82'
-color10='#028c94'
-color11='#039ca4'
-color12='#04acb5'
-color13='#05bbc5'
-color14='#06ccd7'
-color15='#a3b5b8'
+background= "$background"
+foreground= "$foreground"
+color0= "$color0"
+color1= "$color1"
+color2= "$color2"
+color3= "$color3"
+color4= "$color4"
+color5= "$color5"
+color6= "$color6"
+color7= "$color7"
+color8= "$color8"
+color9= "$color9"
+color10= "$color10"
+color11= "$color11"
+color12= "$color12"
+color13= "$color13"
+color14= "$color14"
+color15= "$color15"
 
 accent='#007B82'
 color_red='#ff0000'
