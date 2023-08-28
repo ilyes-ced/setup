486d3a0edd3fe3b05c0b4f83ff23a9b7e67ecfdf
ilyes-ced <random_dude233@proton.me>
2023-08-28 22:08:45 +0100

    more

--- scripts/src/templating.rs
+++ scripts/src/templating.rs
@@ -19,8 +19,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let json_values = read_scheme_json(&file.path()).unwrap();
 
 
-    create_alacritty(json_values.clone());
-    create_polybar(json_values);
+    let _ = create_json(&json_values).unwrap();
+    let _ = create_alacritty(&json_values).unwrap();
+    let _ = create_rofi(&json_values).unwrap();
+    let _ = create_polybar(&json_values).unwrap();
     Ok(())
 
 }
@@ -28,7 +30,43 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 
 
-fn create_alacritty(s: Value) -> Result<(), Box<dyn Error>> {
+fn create_json(s: &Value) -> Result<(), Box<dyn Error>> {
+    let reg = Handlebars::new();
+    let template = fs::read_to_string("templates/json.json").unwrap();
+    let new_json = reg.render_template(
+        &template,
+        &json!({
+            "color0": s["color0"],
+            "color1": s["color1"],
+            "color2": s["color2"],
+            "color3": s["color3"],
+            "color4": s["color4"],
+            "color5": s["color5"],
+            "color6": s["color6"],
+            "color7": s["color7"],
+            "color8": s["color8"],
+            "color9": s["color9"],
+            "color10": s["color10"],
+            "color11": s["color11"],
+            "color12": s["color12"],
+            "color13": s["color13"],
+            "color14": s["color14"],
+            "color15": s["color15"],
+            "background": s["background"],
+            "foreground": s["foreground"],
+            "cursor": s["cursor"],
+        }),
+    )?;
+    let mut file = File::create("themes/active/active.json").unwrap();
+    file.write_all(new_json.as_bytes()).unwrap();
+    Ok(())
+}
+
+
+
+
+
+fn create_alacritty(s: &Value) -> Result<(), Box<dyn Error>> {
     let reg = Handlebars::new();
     let template = fs::read_to_string("templates/alacritty.yml").unwrap();
     let new_json = reg.render_template(
@@ -64,7 +102,7 @@ fn create_alacritty(s: Value) -> Result<(), Box<dyn Error>> {
 
 
 
-fn create_polybar(s: Value) -> Result<(), Box<dyn Error>> {
+fn create_polybar(s: &Value) -> Result<(), Box<dyn Error>> {
     let reg = Handlebars::new();
     let template = fs::read_to_string("templates/colors.ini").unwrap();
     let new_json = reg.render_template(
@@ -98,8 +136,41 @@ fn create_polybar(s: Value) -> Result<(), Box<dyn Error>> {
 
 
 
+fn create_rofi(s: &Value) -> Result<(), Box<dyn Error>> {
+    let reg = Handlebars::new();
+    let template = fs::read_to_string("templates/rofi.rasi").unwrap();
+    let new_json = reg.render_template(
+        &template,
+        &json!({
+            "color0": s["color0"],
+            "color1": s["color1"],
+            "color2": s["color2"],
+            "color3": s["color3"],
+            "color4": s["color4"],
+            "color5": s["color5"],
+            "color6": s["color6"],
+            "color7": s["color7"],
+            "color8": s["color8"],
+            "color9": s["color9"],
+            "color10": s["color10"],
+            "color11": s["color11"],
+            "color12": s["color12"],
+            "color13": s["color13"],
+            "color14": s["color14"],
+            "color15": s["color15"],
+            "background": s["background"],
+            "foreground": s["foreground"],
+            "cursor": s["cursor"],
+        }),
+    )?;
+    let mut file = File::create("themes/active/rofi.rasi").unwrap();
+    file.write_all(new_json.as_bytes()).unwrap();
+    Ok(())
+}
+
+
+
 fn create_gtk() {}
-fn create_rofi() {}
 fn create_i3() {}
 
 
